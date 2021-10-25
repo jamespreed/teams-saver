b47919ac3e062133b633a2fcd6b8eedc9525058f
James <jamesreed.physics@gmail.com>
2021-10-25 16:57:36 +0200

    well...

--- teams_save.js
+++ teams_save.js
@@ -1,11 +1,7 @@
-// find the collapsed replies and expand them
-$('div.conversation-collapsed').find('div.expand-collapse:not(.chevron-expanded)').find('a.ts-collapsed-string').click();
 
-// grab the list of message threads
-var conversation_list = $('div.ts-message-list-item');
 
 function getMessageData(message) {
-    //// gathers the header and body info from a single message in Teams
+    //// Gathers the header and body info from a single message in Teams
     let msg = jQuery(message);
     let msg_id = msg.attr('id');
     let header = msg.find('div.top-row-text-container');
@@ -21,10 +17,10 @@ function getMessageData(message) {
     return data
 }
 
-function getConversationData(conversation) {
-    //// gathers the header and body info from all messages in a conversation
+function getConversationData(conversation_) {
+    //// Gathers the header and body info from all messages in a conversation
     //// thread in Teams
-
+    let conversation = jQuery(conversation_)
     let conversation_id = conversation.children('div.clearfix').attr('id');
     // primary message and replies in the conversation
     let primary_post = conversation.find('div.conversation-start').find('div.thread-body');
@@ -34,7 +30,20 @@ function getConversationData(conversation) {
     var data = getMessageData(primary_post);
     data['conversation_id'] = conversation_id;
 
+    console.log(replies);
     var reply_data = replies.map(getMessageData);
     data['replies'] = reply_data.toArray();
     return data
+}
+
+function saveTeamsConversations() {
+    //// Saves all messages in the current Team view in your browser
+
+    // find the collapsed replies and expand them
+    $('div.conversation-collapsed').find('div.expand-collapse:not(.chevron-expanded)').find('a.ts-collapsed-string').click();
+
+    // grab the list of message threads
+    let conversation_list = $('div.ts-message-list-item');
+    var data = conversation_list.map(getConversationData);
+    return data.toArray()
 }
